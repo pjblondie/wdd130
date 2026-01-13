11f1f72d0f6af9958ab7f820cc3e66df98bccda8
pjblondie <ParkerJack@byui.edu>
2026-01-13 12:01:44 -0700

    added rick

--- ponder/metacss/script.js
+++ ponder/metacss/script.js
@@ -3,10 +3,11 @@ let selectElem = document.getElementById('webdevlist');
 selectElem.addEventListener('change', function(){
     let codeValue = selectElem.value;
     console.log(codeValue);
-
+    let rick = document.getElementById("rick")
     let html_list = document.getElementById("html_list");
     let js_list = document.getElementById("js_list")
     let css_list = document.getElementById("css_list")
+    
     if (codeValue === "js"){
        js_list.classList.add("active") 
        html_list.classList.remove("active")
@@ -22,7 +23,12 @@ selectElem.addEventListener('change', function(){
         js_list.classList.remove("active")
         html_list.classList.remove("active")
     }
-    
+    else if (codeValue === "rick") {
+        js_list.classList.remove("active")
+        html_list.classList.remove("active")
+        css_list.classList.remove("active")
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")
+    }
 })
  
                 
