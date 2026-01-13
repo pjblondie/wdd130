5d355c4376e18bd30a848de357cae9a0383bf19e
pjblondie <ParkerJack@byui.edu>
2026-01-13 11:48:58 -0700

    added java script and css functionality for button, highlights,

--- ponder/metacss/script.js
+++ ponder/metacss/script.js
@@ -1,16 +1,28 @@
 
-const PI = 3.14;
-let radius = 3;
-                  
-const one = 1;
-const two = '2';
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
 
-let course = "CSE131"; //global scope
-if (true) {
-    let student = "John";
-    console.log(course);  //works just fine, course is global
-    console.log(student); //works just fine, it's being accessed within the block
-}
-console.log(course); //works fine, course is global
-console.log(student); //does not work, can't access a block variable outside the block
-                    
+    let html_list = document.getElementById("html_list");
+    let js_list = document.getElementById("js_list")
+    let css_list = document.getElementById("css_list")
+    if (codeValue === "js"){
+       js_list.classList.add("active") 
+       html_list.classList.remove("active")
+       css_list.classList.remove("active")
+    }
+    else if (codeValue === "html"){
+        html_list.classList.add("active")
+        js_list.classList.remove("active")
+        css_list.classList.remove("active")
+    }   
+    else if (codeValue === "css"){
+        css_list.classList.add("active")
+        js_list.classList.remove("active")
+        html_list.classList.remove("active")
+    }
+    
+})
+ 
+                
